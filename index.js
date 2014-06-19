d91b3735524761e0de0956a2c0909751d02d58e4
Tyler Cross <tcross@bandwidth.com>
2014-06-19 08:53:32 -0400

    Make plugin follow standards of hapi 6.0.x plugin interface - updated plugin to throw an exception if mongo URL not set in options - updated tests to cover new options

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
+var Hoek = require('hoek');
+
 var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 var ObjectID = mongodb.ObjectID;
 
+var defaults = {
+  settings: {
+
+  }
+};
+
 exports.register = function (plugin, options, next) {
-  MongoClient.connect(options.url, options.settings || {}, function (err, db) {
+  Hoek.assert(!!options.url, 'MongoDB URL is required');
+  options = Hoek.applyToDefaults(defaults, options);
+
+  MongoClient.connect(options.url, options, function (err, db) {
     if (err) return next(err);
+
     plugin.expose('db', db);
     plugin.expose('lib', mongodb);
     plugin.expose('ObjectID', ObjectID);
+
     next();
   });
-}
+};
+
+exports.register.attributes = {
+  pkg: require('./package.json')
+};
--- test/connection.js
+++ test/connection.js
@@ -2,23 +2,75 @@ var Hapi = require('hapi');
 var assert = require('assert');
 
 describe('Hapi server', function() {
-  before(function() {
-    this.server = new Hapi.Server();
+  var server = null;
+
+  beforeEach(function() {
+    server = new Hapi.Server();
   });
-  it('should be able to register plugin', function(done) {
-    this.server.pack.require({
-      '..': {
+
+  afterEach(function () {
+    server = null;
+  });
+
+  it('should be able to register plugin with just URL', function(done) {
+    server.pack.register({
+        plugin: require('../'),
+        options: {
+            url: 'mongodb://localhost:27017'
+        }
+     }, function (err) {
+         assert(err === undefined, 'An error was thrown but should not have been.');
+         done();
+     });
+  });
+
+  it('should be able to register plugin with URL and settings', function(done) {
+    server.pack.register({
+        plugin: require('../'),
+        options: {
+            url: 'mongodb://localhost:27017',
+            settings: {
+              db : {
+                native_parser : false
+              }
+            }
+        }
+     }, function (err) {
+         assert(err === undefined, 'An error was thrown but should not have been.');
+         done();
+     });
+  });
+
+  it('should not be able to register the plugin without URL', function(done) {
+    assert.throws(function () {
+        server.pack.register({
+            plugin: require('../')
+        }, function () {
+
+        });
+      },
+      /MongoDB URL is required/
+    );
+
+    done();
+  });
+
+  it('should be able to find the plugin exposed objects', function(done) {
+    server.pack.register({
+      plugin: require('../'),
+      options: {
         url: 'mongodb://localhost:27017'
       }
-    }, done);
-  });
-  it('should be able to find the plugin exposed objects', function(done) {
-    this.server.route({ method: 'GET', path: '/', handler: function(request, reply) {
-      assert(request.server.plugins['hapi-mongodb'].db, 'Could not find db object');
-      assert(request.server.plugins['hapi-mongodb'].lib, 'Could not find mongodb library');
-      assert(request.server.plugins['hapi-mongodb'].ObjectID, 'Could not find mongodb ObjectID');
-      done();
-    }});
-    this.server.inject({ method: 'GET', url: '/' }, function() {});
+    }, function (err) {
+      server.route({ method: 'GET', path: '/', handler: function(request, reply) {
+        assert(request.server.plugins['hapi-mongodb'].db, 'Could not find db object');
+        assert(request.server.plugins['hapi-mongodb'].lib, 'Could not find mongodb library');
+        assert(request.server.plugins['hapi-mongodb'].ObjectID, 'Could not find mongodb ObjectID');
+
+        done();
+      }});
+
+      server.inject({ method: 'GET', url: '/' }, function() {});
+    });
   });
 });
