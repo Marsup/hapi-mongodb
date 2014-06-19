8e2efdbdfc4b2e9b17add99a13875654ae722d62
Tyler Cross <tcross@bandwidth.com>
2014-06-19 08:53:32 -0400

    Make plugin follow standards of hapi 6.0.x plugins

--- index.js
+++ index.js
@@ -3,11 +3,24 @@ var MongoClient = mongodb.MongoClient;
 var ObjectID = mongodb.ObjectID;
 
 exports.register = function (plugin, options, next) {
-  MongoClient.connect(options.url, options.settings || {}, function (err, db) {
-    if (err) return next(err);
+  options.url = options.url || 'mongodb://localhost:27017';
+
+  MongoClient.connect(options.url, options.settings, function (err, db) {
+    if (err) {
+      plugin.log([ 'hapi-mongodb', 'error' ], 'Error connecting to MongoDB');
+      return next(err);
+    }
+
     plugin.expose('db', db);
     plugin.expose('lib', mongodb);
     plugin.expose('ObjectID', ObjectID);
+
+    plugin.log([ 'hapi-mongodb', 'info' ], 'MongoClient connection created');
+
     next();
   });
-}
+};
+
+exports.register.attributes = {
+  pkg: require('./package.json')
+};
